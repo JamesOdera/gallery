1232150d56aa63dafd153f24e10b28a7e7c59d27
JamesOdera <nyangoma.odera@gmail.com>
2020-03-02 10:31:12 +0300

    add a url to get the past dates

--- album/views.py
+++ album/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.http  import HttpResponse
+from django.http  import HttpResponse,Http404
 import datetime as dt
 
 # Create your views here.
@@ -31,3 +29,22 @@ def convert_dates(dates):
     # Returning the actual day of the week
     day = days[day_number]
     return day
+
+def past_days_album(request,past_date):
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>Image for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)

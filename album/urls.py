9bc660397e50ae42ac9fe89375256444336de52a
JamesOdera <nyangoma.odera@gmail.com>
2020-03-02 11:43:09 +0300

    update our urls pyfile to enable our app load automatically

--- album/urls.py
+++ album/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url(r'^$',views.welcome,name = 'welcome'),
-    url(r'^today/$',views.album_today,name='albumToday'),
+    url(r'^$',views.album_today,name='albumToday'),
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_album,name = 'pastAlbum')
 ]

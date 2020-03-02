1b1ae5a3775cc7ac7daf26aefd0926a5f1839321
JamesOdera <nyangoma.odera@gmail.com>
2020-03-02 10:27:21 +0300

    modify the date to get the date content is added

--- album/urls.py
+++ album/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+    url(r'^$',views.welcome,name = 'welcome'),
+    url(r'^today/$',views.album_of_day,name='albumToday')
 ]
--- album/views.py
+++ album/views.py
@@ -2,7 +2,32 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http  import HttpResponse
+import datetime as dt
 
 # Create your views here.
 def welcome(request):
     return HttpResponse('Galeery')
+
+def album_of_day(request):
+    date = dt.date.today()
+
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1> Image for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
+
+def convert_dates(dates):
+
+    # Function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day
